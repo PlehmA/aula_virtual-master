8973b0cb4485e872e19843f269ef57c4f43ee7a4
julindres <tengo3cmdepito@gmail.com>
2016-09-29 19:04:00 -0300

    fghfgh

--- send.php
+++ send.php
@@ -1,6 +1,20 @@
 session_start();
 if (isset($_POST['enviar']) && $_POST['captcha'] && $_POST['captcha'] !="" && $_SESSION['code']==$_POST['captcha'])
+{
 $nombre=$_POST['nombre'];
 $mail=$_POST['mail'];
 $empresa =$_POST['empresa'];
-$header = "From:" . $mail . "\r\n";
+$header .= "From:" . $mail . "\r\n";
+$header .= "Mime Version 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ", de la 
+empresa " . $empresa . " \r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = "info@micorreo.com.ar";
+$asunto = "Pedido de Informacion de:" . $nombre;
+
+}
